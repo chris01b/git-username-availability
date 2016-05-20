69f403eea43d406f8569e524215a7a2770f92357
BKmaster2580 <nova6ers@gmail.com>
2016-05-19 21:03:55 -0400

    Added detection of reserved usernames

--- names.py
+++ names.py
@@ -6,21 +6,24 @@ parser.add_argument('usernames', help='username to check')
 parser.add_argument('--source','-s', action='store_true', help='check usernames from file')
 args = parser.parse_args()
 
+reserved = [line.rstrip('\n') for line in open("reserved-usernames.txt")]
+
 def isUp(username):
 	statusCode = requests.get('https://api.github.com/users/' + username).status_code
-	if statusCode == 404:
-		code = False
+	if username in reserved:
+			print("Taken")
+	elif statusCode == 404:
 		print("Available")
 	elif statusCode == 200:
-		code = True
 		print("Taken")
 	elif statusCode != 404 or 200:
 		raise ValueError("Unexpected statuscode")
-	return code
 
 if args.source:
 	content = [line.rstrip('\n') for line in open(args.usernames)]
 	for username in content:
+		print(username.upper() + ":")
 		isUp(username)
+		print("")
 else:
 	isUp(args.usernames)
